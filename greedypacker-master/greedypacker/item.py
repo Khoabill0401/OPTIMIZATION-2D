783b1b99dc33f77344bb5f3a4d56318194161d60
Khoabill0401 <khoabill0401@gmail.com>
2022-01-08 16:35:45 +0700

    first commit

---
+++ greedypacker-master/greedypacker/item.py
@@ -0,0 +1,26 @@
+"""
+2D Item class.
+"""
+class Item:
+    """
+    Items class for rectangles inserted into sheets
+    """
+    def __init__(self, width, height,
+                 CornerPoint: tuple = (0, 0),
+                 rotation: bool = True) -> None:
+        self.width = width
+        self.height = height
+        self.x = CornerPoint[0]
+        self.y = CornerPoint[1]
+        self.area = self.width * self.height
+        self.rotated = False
+        self.id = 0
+
+
+    def __repr__(self):
+        return 'Item(width=%r, height=%r, x=%r, y=%r)' % (self.width, self.height, self.x, self.y)
+
+
+    def rotate(self) -> None:
+        self.width, self.height = self.height, self.width
+        self.rotated = False if self.rotated == True else True
